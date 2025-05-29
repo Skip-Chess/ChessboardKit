6eb28d6921d6ca8297d202200a5541330d049783
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-05-29 17:15:31 -0400

    Fix branch reference and use swift-tools-version: 6.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://source.skip.tools/skip.git", from: "1.5.18"),
         .package(url: "https://source.skip.tools/skip-fuse-ui.git", "0.0.0"..<"2.0.0"),
-        .package(url: "https://github.com/Skip-Chess/ChessKit.git", branch: "main"),
+        .package(url: "https://github.com/Skip-Chess/ChessKit.git", branch: "develop"),
     ],
     targets: [
         .target(name: "ChessboardKit",
